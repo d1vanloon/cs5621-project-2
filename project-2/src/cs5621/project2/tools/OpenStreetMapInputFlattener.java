0ad75980b9d2c648f14bf974369683066c250a5d
David Van Loon <david.van.loon@outlook.com>
2015-04-17 10:04:32 -0500

    Initial commit of OpenStreetMapInputFlattener

---
+++ project-2/src/cs5621/project2/tools/OpenStreetMapInputFlattener.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package cs5621.project2.tools;
+
+/**
+ * Flattens OpenStreetMap data into a format more easily interpreted in a
+ * line-by-line manner.
+ * 
+ * @author David Van Loon
+ *
+ */
+public class OpenStreetMapInputFlattener {
+
+}
