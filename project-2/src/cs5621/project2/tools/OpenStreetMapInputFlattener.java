a860e4dbf51fdd0738397c1f797aed3c78e2c1f9
d1vanloon <david.van.loon@outlook.com>
2015-04-17 20:42:05 -0500

    Properly flattens ways Only keeps highway and name tags

--- project-2/src/cs5621/project2/tools/OpenStreetMapInputFlattener.java
+++ project-2/src/cs5621/project2/tools/OpenStreetMapInputFlattener.java
@@ -21,12 +21,20 @@ import java.util.LinkedList;
  */
 public class OpenStreetMapInputFlattener {
 
+	private static final String K_NAME = "k=\"name\"";
+	private static final String K_HIGHWAY = "k=\"highway\"";
+	private static final String ND = "<nd";
+	private static final String TAG = "<tag";
+	private static final String WAY_END = "</way";
+	private static final String WAY = "<way";
 	File inputFile;
 	File outputFile;
 	BufferedReader reader;
 	OutputStream out;
 	String currentLine;
-	LinkedList<String> currentWay;
+	LinkedList<String> currentWayNodes;
+	String currentWayAttributes;
+	String currentWayTags;
 
 	/**
 	 * Initializes a new OpenStreetMapInputFlattener
@@ -44,6 +52,11 @@ public class OpenStreetMapInputFlattener {
 				Files.newInputStream(inputFile.toPath())));
 		this.out = new BufferedOutputStream(Files.newOutputStream(outputFile
 				.toPath()));
+
+		this.currentWayNodes = new LinkedList<String>();
+		this.currentLine = "";
+		this.currentWayAttributes = "";
+		this.currentWayTags = "";
 	}
 
 	/**
@@ -54,27 +67,71 @@ public class OpenStreetMapInputFlattener {
 	public int flatten() {
 		int processedLines = 0;
 		currentLine = "";
+		boolean inWay = false;
+		String trimmedLine;
 
-		// Stream through the file until a way is encountered
-		while (!currentLine.startsWith("<way")) {
-			try {
-				// Read a line
-				currentLine = reader.readLine();
-				// Exit the loop if there are no more lines remaining
-				if (currentLine == null) {
-					break;
+		try {
+			while ((currentLine = reader.readLine()) != null) {
+				trimmedLine = currentLine.trim();
+				// If we're processing the body of a way
+				if (inWay) {
+					// If the current line is a node
+					if (trimmedLine.startsWith(ND)) {
+						// Store the current node
+						currentWayNodes.add(trimmedLine);
+					} else
+					// If the current line is a highway or name tag
+					if ((trimmedLine.startsWith(TAG))
+							&& (trimmedLine.contains(K_HIGHWAY) || trimmedLine
+									.contains(K_NAME))) {
+						// Append the tag to the current way tags
+						currentWayTags += " " + trimmedLine;
+					} else
+					// If the current line is the end of the way
+					if (trimmedLine.startsWith(WAY_END)) {
+						// For each node
+						while (!currentWayNodes.isEmpty()) {
+							// Get the node and remove it from the list
+							String node = currentWayNodes.removeFirst();
+							// Output the node with the way data in front and
+							// the way tags
+							// behind
+							out.write((currentWayAttributes + " " + node + currentWayTags)
+									.getBytes());
+							// ... and a new line
+							out.write(System.lineSeparator().getBytes());
+						}
+						// We're done with the way
+						inWay = false;
+						currentWayTags = "";
+					}
+				} else
+				// If we're currently at the start of a way
+				if (trimmedLine.startsWith(WAY)) {
+					// Set the current way attributes to the newly discovered
+					// way
+					currentWayAttributes = trimmedLine;
+					// We're now processing a way
+					inWay = true;
+				} else
+				// If we're looking at any other line
+				{
+					// Just output the line
+					out.write(trimmedLine.getBytes());
+					// ... and a new line
+					out.write(System.lineSeparator().getBytes());
 				}
-				// ... and immediately write it to the output file
-				out.write(currentLine.getBytes());
-				// With a new line for good measure
-				out.write(System.lineSeparator().getBytes());
-				out.flush();
-				// Increment the number of processed lines
 				processedLines++;
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Flush the output buffer
+		try {
+			out.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 		return processedLines;
