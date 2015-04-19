8f1811f25476a2d70872183f79fcdfb3f3c18dd3
d1vanloon <david.van.loon@outlook.com>
2015-04-19 14:32:58 -0500

    Added Job1Mapper.java skeleton class based on suggested job descriptions

---
+++ project-2/src/cs5621/project2/job1/Job1Mapper.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package cs5621.project2.job1;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ * Job 1 mapper class. The mapper function for Job 1 is tasked with reading the
+ * flattened input data and grouping data by node ID.
+ * 
+ * @author David Van Loon
+ *
+ */
+public class Job1Mapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+
+	/* (non-Javadoc)
+	 * @see org.apache.hadoop.mapreduce.Mapper#map(java.lang.Object, java.lang.Object, org.apache.hadoop.mapreduce.Mapper.Context)
+	 */
+	@Override
+	protected void map(LongWritable key, Text value,
+			Mapper<LongWritable, Text, LongWritable, Text>.Context context)
+			throws IOException, InterruptedException {
+		super.map(key, value, context);
+	}
+
+}
