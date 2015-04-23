43f1b8f4ed05d7fcaeb933d89ad067116e138b6c
Brad <thecoolestname36@live.com>
2015-04-22 19:57:37 -0500

    Comparator Class Additions

---
+++ project-2/src/cs5621/project2/job2/KeyGroupingComparator.java
@@ -0,0 +1,29 @@
+package cs5621.project2.job2;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.io.Text;
+
+public class KeyGroupingComparator extends WritableComparator{
+
+	public KeyGroupingComparator() {
+		super(Text.class, true);
+		
+	}
+	
+	@Override
+	public int compare(WritableComparable k1, WritableComparable k2) {
+		
+		int index = 0;
+		// Pull wayID and nodeID from k1
+		index = ((Text) k1).toString().indexOf('\t');
+		Text k1WayID = new Text(((Text) k1).toString().substring(0, index));
+		
+		// Pull wayID and nodeID from k2 
+		index = ((Text) k2).toString().indexOf('\t');
+		Text k2WayID = new Text(((Text) k2).toString().substring(0, index));
+	
+		// Returns 0 if equal
+		return k2WayID.compareTo(k1WayID);
+	}
+}
