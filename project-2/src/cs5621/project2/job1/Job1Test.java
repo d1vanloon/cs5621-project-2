b594dd079ff5073094b867f56f1f62f0db05cdba
David Van Loon <david.van.loon@outlook.com>
2015-04-27 14:00:46 -0500

    Modified Job1Test

--- project-2/src/cs5621/project2/job1/Job1Test.java
+++ project-2/src/cs5621/project2/job1/Job1Test.java
@@ -2,47 +2,35 @@ package cs5621.project2.job1;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
-
+import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Job1Test {
 	public static void main(String[] args) throws Exception {
-		
 		Configuration conf = new Configuration();
-		Job job = new Job(conf, "Job");
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
+
+		Job job = new Job(conf, "Job1Test");
 
 		job.setJarByClass(Job1Test.class);
-
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
-		job.setOutputKeyClass(Text.class);
-
-		job.setOutputValueClass(IntWritable.class);
-
+		
 		job.setMapperClass(Job1Mapper.class);
-
 		job.setReducerClass(Job1Reducer.class);
+		
+		job.setMapOutputKeyClass(LongWritable.class);
+		job.setMapOutputValueClass(Text.class);
 
-		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
 
-		job.setOutputFormatClass(TextOutputFormat.class);
-
-		job.setNumReduceTasks(128);
-
-		FileInputFormat.addInputPath(job, new Path(args[3]));
-
-		FileOutputFormat.setOutputPath(job, new Path(args[3] + "/sampleOutput/"));
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 
 		job.waitForCompletion(true);
-		
 	}
-	
-	
 }
