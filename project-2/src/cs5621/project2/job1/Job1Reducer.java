71f0fb16cd35fa9d321ce916e091de8c1a6224b9
jordanross1992 <rossx624@d.umn.edu>
2015-04-27 11:02:27 -0500

    Second Reducer partial implementation

--- project-2/src/cs5621/project2/job1/Job1Reducer.java
+++ project-2/src/cs5621/project2/job1/Job1Reducer.java
@@ -1,5 +1,6 @@
 package cs5621.project2.job1;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
@@ -11,8 +12,19 @@ public class Job1Reducer extends Reducer<LongWritable, Text, Text, Text> {
 			 Context context) throws IOException, InterruptedException {
 		 //Strings to contain the value information that will be split based on blank spaces
 		 //information will either be way information or a pair of coordinates
-		 String [] coordinates = null;
-		 String [] wayInfo = null;
+		String [] coordinates = null;
+		String[] wayInfo = null;
+		String isIntersection = "";
+		ArrayList<String> master = new ArrayList<String>();
+		//check to see if we have an intersection...
+		if(isIntersection(values)){
+			isIntersection = "1";
+		 }
+		 //else it is not an intersection, append '0' to the way info
+		 else{
+			 isIntersection = "0";
+		 }
+		
 		 for(Text value:values){
 			 //if the first character of the string is a 'w' we have a way
 			 if(value.charAt(0) == 'w'){
@@ -22,28 +34,23 @@ public class Job1Reducer extends Reducer<LongWritable, Text, Text, Text> {
 			 else{
 				 coordinates = value.toString().split(" ");
 			 }
-			 // concatenate the coordinates to the wayInfo
-			 wayInfo.toString().concat(" " + coordinates[0]);
-			 wayInfo.toString().concat(" " + coordinates[1]);
-			 //if it is an intersection append '1' to the way info
-			 if(isIntersection(values)){
-				 wayInfo.toString().concat(" " + '1');
+			 //if we have coordinates and a way entry concatenate them, store them in a string and add it to the array list.
+			 if(coordinates != null && wayInfo != null){
+				 String entry = wayInfo[0] + " " + wayInfo[1] + " " +wayInfo[2] + " " + wayInfo[3] + " " + coordinates[0]  + " " + coordinates[1];
+				 master.add(entry);
 			 }
-			 //else it is not an intersection, append '0' to the way info
-			 else{
-				 wayInfo.toString().concat(" " + '0');
-			 }
-			 //newKey should contain WayID, Way Name, and the Node Index
-			 //newValue should contain Node Latitude, Node Longitude,  Node Index, and Intersection Flag
-			 Text newKey = new Text(wayInfo[0] + wayInfo[3] + wayInfo[2]);
-			 Text newValue = new Text(wayInfo[4] + " " + wayInfo[5] + " " + wayInfo[2] + " " + wayInfo[6]);
 			 
-			 //Write the key values
-			 context.write(newKey, newValue);
 		 }
+		 for(String entries:master){
+	     if(entries != null){
+	    	String [] info = entries.toString().split(" ");
+		 	Text newKey = new Text(info[0] + info[3] + info[2]);
+		 	Text newValue = new Text(info[4] + " " + info[5] + " " + wayInfo[2] + " " + isIntersection);
 		 
-		 
-		
+		 	//Write the key values
+		 	context.write(newKey, newValue);
+	     	}
+	     }
 	 }//end reducer function
 
 	 
