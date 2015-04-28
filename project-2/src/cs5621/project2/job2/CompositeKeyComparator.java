249948db88bb6a2cf610488207493a3283f0e0d0
Brad <thecoolestname36@live.com>
2015-04-27 19:18:07 -0500

    Comparator & Partitioner Modification

--- project-2/src/cs5621/project2/job2/CompositeKeyComparator.java
+++ project-2/src/cs5621/project2/job2/CompositeKeyComparator.java
@@ -15,20 +15,43 @@ public class CompositeKeyComparator extends WritableComparator {
 	@Override
 	public int compare(WritableComparable k1, WritableComparable k2) {
 		int index = 0;
+		Text k1NodeIndex = new Text();
+		Text k2NodeIndex = new Text();
+		IntWritable k1NodeIndexNum = new IntWritable();
+		IntWritable k2NodeIndexNum = new IntWritable();
 		
-		// Pull wayID and nodeID from k1
+		
+		// Pull nodeIndex from k1
 		index = ((Text) k1).toString().indexOf('\t');
-		Text k1NodeID = new Text(((Text) k1).toString().substring(index));
-		IntWritable k1WayIDNum = new IntWritable(Integer.valueOf(k1NodeID.toString()));
 		
-		// Pull wayID and nodeID from k2 
+		try {
+			k1NodeIndex = new Text(((Text) k1).toString().substring(index));
+			try {
+				k1NodeIndexNum = new IntWritable(Integer.valueOf(k1NodeIndex.toString()));
+			} catch (NumberFormatException e) {
+				System.out.println("CompositeKeyComparator Error: " + e.getMessage());
+			}
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("CompositeKeyComparator Error: " + e.getMessage());
+		}
+		
+		// Pull nodeID from k2 
 		index = ((Text) k2).toString().indexOf('\t');
-		Text k2NodeID = new Text(((Text) k2).toString().substring(index));
-		IntWritable k2WayIDNum = new IntWritable(Integer.valueOf(k2NodeID.toString()));
+		
+		try {
+			k2NodeIndex = new Text(((Text) k2).toString().substring(index));
+			try {
+				k2NodeIndexNum = new IntWritable(Integer.valueOf(k2NodeIndex.toString()));
+			} catch (NumberFormatException e) {
+				System.out.println("CompositeKeyComparator Error: " + e.getMessage());
+			}
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("CompositeKeyComparator Error: " + e.getMessage());
+		}
 		
 		// compareTo:: return (thisValue<thatValue ? -1 : (thisValue==thatValue ? 0 : 1));
 		// Returns 1 if k2 < k1 = -1, else 1
-		return  k2WayIDNum.compareTo(k1WayIDNum);
+		return  k2NodeIndexNum.compareTo(k1NodeIndexNum);
 		
 	}
 
